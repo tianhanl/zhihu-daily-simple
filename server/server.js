14e0f60e10729e55bef4725b43132ac2c8437f7c
tianhanl <tianhanl@usc.edu>
2017-07-14 19:54:28 +0800

    setting up the server

--- server/server.js
+++ server/server.js
@@ -1,2 +1,47 @@
 const express = require('express');
 const path = require('path');
+const crawler = require('./crawler');
+
+let app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/api', function(req, res) {
+    res.json({
+        '/list': 'get the list of stories',
+        '/story': 'get the story content with id'
+    });
+});
+
+app.get('/api/list', function(req, res) {
+    crawler
+        .requestLatestStoryList()
+        .then(function(stories) {
+            res.json(stories);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+});
+
+app.get('/api/stories', function(req, res) {
+    let id = req.params.id;
+    crawler
+        .requestStory(id)
+        .then(function(story) {
+            res.json(story);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+});
+
+app.use(function(req, res) {
+    res.status('404');
+    res.send('404, the page is not exist');
+    res.end();
+})
+
+app.listen(app.get('port'), function() {
+    console.log('The server has been started');
+})
